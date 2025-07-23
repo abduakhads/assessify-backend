a67d290e923958fed7df8e483389b6b93ef51075
abduakhads <160651255+abduakhads@users.noreply.github.com>
2025-07-24 00:51:11 +0200

    rename

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for assessly project.
+ASGI config for assessify project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for assessly project.
+WSGI config for assessify project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
